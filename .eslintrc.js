3f461991c7295a95973e77b849f3402538ac6e28
Rishit Pandey <rishpandey8097@gmail.com>
2021-12-13 20:45:23 +0530

    FEATURE: eslint rules

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	root: true,
+	files: ['*.js'],
+	rules: {
+		semi: ['error', 'always'],
+		quotes: ['error', 'single'],
+		'no-unused-expressions': 'error'
+	},
+	extends: 'eslint:recommended',
+	plugins: []
+};
